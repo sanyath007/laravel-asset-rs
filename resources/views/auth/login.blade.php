fda36396de82be3731f4e3093fe02e3f94c6a2eb
sanyath007 <sanyath007@gmail.com>
2020-10-30 09:28:59 +0700

    Clear unneccessary css and js files of login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,83 +8,17 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Accounts Payable System</title>
+    <title>Asset Management System</title>
 
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
-    <!-- Compiled and minified CSS -->
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css"> -->
-    <!-- <link href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet"/> -->
-    <link rel="stylesheet" href="{{ asset('/bower/fullcalendar/dist/fullcalendar.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/bower/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/bower/ng-tags-input/ng-tags-input.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/bower/AngularJS-Toaster/toaster.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/bower/angular-xeditable/dist/css/xeditable.css') }}">
-    <!-- <link href="{{ asset('/css/bootstrap-tagsinput.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/bootstrap-tagsinput-typeahead.css') }}" rel="stylesheet"> -->
-    <!-- <link rel="stylesheet" href="{{ asset('/css/theme.css') }}"> -->
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{ asset('/css/ionicons.min.css') }}">
-    <!-- jvectormap -->
-    <link rel="stylesheet" href="{{ asset('/css/jquery-jvectormap.css') }}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('/css/AdminLTE.min.css') }}">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-        folder instead of downloading all of them to reduce the load. -->
+    <!-- AdminLTE Skins. Choose a skin from the css/skins 
+         folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="{{ asset('/css/skins/_all-skins.min.css') }}">
-
     <!-- Fonts -->
-    <!-- <link rel='stylesheet' href='//fonts.googleapis.com/css?family=Roboto:400,300' type='text/css'> -->
+    <link rel='stylesheet' href='//fonts.googleapis.com/css?family=Roboto:400,300' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    
-    <!-- Scripts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="{{ asset('/bower/angular/angular.min.js') }}"></script>
-    <script src="{{ asset('/bower/moment/moment.js') }}"></script>
-    <script src="{{ asset('/bower/fullcalendar/dist/fullcalendar.min.js') }}"></script>
-    <script src="{{ asset('/bower/fullcalendar/dist/locale/th.js') }}"></script>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script> -->
-    <script src="{{ asset('/bower/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') }}"></script>
-    <script src="{{ asset('/bower/ng-tags-input/ng-tags-input.min.js') }}"></script>
-    <script src="{{ asset('/bower/angular-animate/angular-animate.min.js') }}"></script>
-    <script src="{{ asset('/bower/AngularJS-Toaster/toaster.min.js') }}"></script>
-    <script src="{{ asset('/bower/angular-xeditable/dist/js/xeditable.js') }}"></script>
-    <script src="{{ asset('/bower/angular-modal-service/dst/angular-modal-service.min.js') }}"></script>
-    <script src="{{ asset('/bower/underscore/underscore-min.js') }}"></script>
-    <!-- <script src="{{ asset('/bower/axios/dist/axios.min.js') }}"></script> -->
-    <!-- <script src="{{ asset('/js/bootstrap-tagsinput.min.js') }}"></script>
-    <script src="{{ asset('/js/bootstrap-tagsinput-angular.min.js') }}"></script> -->
-    <script src="{{ asset('/js/jquery-ui.min.js') }}"></script>
-    <script src="{{ asset('/js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
-    <script src="{{ asset('/js/daterangepicker.js') }}"></script>
-    <script src="{{ asset('/js/bootstrap-datepicker.min.js') }}"></script>
-    <script src="{{ asset('/js/jquery.knob.min.js') }}"></script>
-    <script src="{{ asset('/js/fastclick.js') }}"></script>
-
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="http://code.highcharts.com/highcharts-more.js"></script>
-    
-    <!-- AdminLTE App -->
-    <script src="{{ asset('/js/adminlte.min.js') }}"></script>
-
-    <!-- AngularJS Components -->
-    <script src="{{ asset('/js/main.js') }}"></script>
-    <script src="{{ asset('/js/controllers/mainCtrl.js') }}"></script>
-    <!-- <script src="{{ asset('/js/reserveCtrl.js') }}"></script>
-    <script src="{{ asset('/js/assignCtrl.js') }}"></script>
-    <script src="{{ asset('/js/maintainedCtrl.js') }}"></script>
-    <script src="{{ asset('/js/insuranceCtrl.js') }}"></script>
-    <script src="{{ asset('/js/taxCtrl.js') }}"></script>
-    <script src="{{ asset('/js/fuelCtrl.js') }}"></script>
-    <script src="{{ asset('/js/reportCtrl.js') }}"></script>
-    <script src="{{ asset('/js/directives/highcharts.js') }}"></script>
-    <script src="{{ asset('/js/services/report.js') }}"></script> -->
-
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="{{ asset('/js/services/dashboard.js') }}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{ asset('/js/services/demo.js') }}"></script>
 
 </head>
 <body class="hold-transition skin-blue sidebar-mini" ng-app="app" ng-controller="mainCtrl">
@@ -163,5 +97,11 @@
         <!-- Footer -->
 
     </div><!-- /.wrapper -->
+
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('/js/adminlte.min.js') }}"></script>
 </body>
 </html>
